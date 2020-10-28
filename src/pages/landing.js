ae80d687e9b7a939a57d5743733504fd4db436b3
Lucas H Trevelin <lhtrevelin@gmail.com>
2020-10-28 20:40:54 -0300

    versao mais atual e com uma so home

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,8 +1,6 @@
 import "../styles/global.css";
 import 'fontsource-roboto';
-import {Avatar, Button, Card, Container, IconButton, Paper} from '@material-ui/core';
-import Lucas from "../images/Lucas.png";
-import Lucas2 from "../images/Lucas2.jpeg";
+import {Avatar, Box, IconButton, Paper} from '@material-ui/core';
 import Lucas3 from "../images/Lucas3.jpeg";
 import WorkIcon from '@material-ui/icons/WorkOutline';
 import Person from '@material-ui/icons/PersonOutline';
@@ -12,10 +10,6 @@ import Git from '@material-ui/icons/GitHub';
 import LinkedIn from '@material-ui/icons/LinkedIn';
 import Download from '@material-ui/icons/GetAppOutlined'
 import "../styles/landing.css"
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 
 
 function landing() {
@@ -27,75 +21,57 @@ function landing() {
           Bem vindo ao meu Portifólio!
         </h1>  
         <hr/>
-        <div className="backgAvatar" style={{backgroundImage:"url(https://img.freepik.com/free-vector/application-development-banner_1325-399.jpg?size=626&ext=jpg", margin:'50px auto' , height:'25vh', width: '100%', borderRadius:'60px', backgroundRepeat:'round'}}>
+        <div className="headerContentContainer" style={{margin:"0"}}>
+          <Box boxShadow={3} boxSizing className="backgAvatar" >
+            <Avatar variant="round" alt="Lucas" src={Lucas3} style={{'width': '180px', 'height':'auto','margin': '0 auto', 'border':'5px solid black'}}/>
+            <div className="whitebox">
+              <p style={{'color': "black", 'fontSize': '1.5em', margin: 'auto', padding: '10px'}}>
+                <em>
+                  <div className="person" style={{'display':'flex', 'flexDirection': 'row', 'justifyContent': 'center'}}>
+                    <Person/>
+                    <strong>Lucas Henrique Trevelin</strong>
+                  </div>
+                  <div className="work" style={{'display':'flex', 'flexDirection': 'row', 'justifyContent': 'center', 'marginTop': '10px'}}>
+                    <WorkIcon/>
+                    Web Developer
+                  </div>
+                </em>
+              </p>
+            </div>
+          </Box>
+          <Box boxShadow={3} boxSizing className="backgAbout" >
+          <Paper elevation={5} style={{ backgroundColor: '#272c34', height: 'auto', width:'90%', margin:"auto" , 'border-radius': '30px', border:'3px solid grey', padding:'10px', overflow:"scroll", minWidth:'300px',}}> 
+            <h1 style={{ color: 'yellow', 'margin': '20px 20px', textAlign:'center'}}>Quem sou eu?</h1>
+            <p style={{width:'95%', height:'auto', color:'white', lineHeight:'1.6', textAlign:'start', textIndent: '40px', padding:'10px', fontSize:'1.25em', overflow:'auto'}}>Sou graduado em Física Biomolecular pela USP-São Carlos, fiz 1 ano de mestrado com bolsa CAPES porém vi que tenho pontos mais fortes para atuação no mundo empresarial. Então atuei por 3 anos como analista de investimentos e riscos Jr. numa empresa de soluções financeiras que atua através de tecnologia e depois em uma consultoria financeira como consultor independente atuando com prospecção e suporte de clientes. Sou uma pessoa bem comunicativa, que contribui como team player e que busca aprender e evoluir em conhecimento. 
+            </p>
+          </Paper>
+          </Box>
+          <Box boxShadow={3} boxSizing className="backgProjects" >
+          <Paper elevation={5} style={{ backgroundColor: '#272c34', height: '90%', width:'90%', margin:"auto" , 'border-radius': '30px', border:'3px solid grey', padding:'10px', overflow:"hidden", minWidth:'300px',}}> 
+            <h1 style={{ color: 'greenyellow', 'margin': '20px', textAlign:'center'}}>Projetos</h1>
+            <p style={{width:'95%', margin:'20px' , height:'auto', color:'white', lineHeight:'1.6', textAlign:'start', padding:'10px', fontSize:'1.25em', overflow:'auto', textAlign:'center'}}> Insira seu projeto aqui! 
+            </p>
+          </Paper>
+          </Box>
         </div>
       </div>
     </header>
-    <body style={{'background':'linear-gradient(0deg, rgba(8,8,8,0.6544818611038166) 0%, rgba(0,0,0,0.8701681356136204) 69%, rgba(0,0,0,1) 100%)', 'width':'100vw' }}>
-    <Avatar variant="round" alt="Lucas" src={Lucas3} style={{'width': '17vw', 'height':'auto', 'top': '-7vw','margin': '0 auto', 'position': 'relative', 'border':'5px solid black'}}/>
-    <p style={{'color': "white", 'fontSize': '1.25em','margin':'auto auto 5vh auto'}}>
-          <em>
-            <div className="person" style={{'display':'flex', 'flexDirection': 'row', 'justifyContent': 'center'}}>
-              <Person/>
-              <strong>Lucas Henrique Trevelin</strong>
-            </div>
-            <div className="work" style={{'display':'flex', 'flexDirection': 'row', 'justifyContent': 'center', 'marginTop': '10px'}}>
-              <WorkIcon/>
-              Web Developer
-            </div>
-          </em>
-        </p>
-    <Container maxWidth="xl">
-      <div style={{ backgroundColor: '#cccccc', height: '66vh', 'border-radius': '60px' , margin: ''}}>
-        <Paper elevation={5} style={{ backgroundColor: '#272c34', height: '26vh', width:'90%', margin:"auto" , 'border-radius': '30px', border:'3px solid grey'}}> 
-          <h1 style={{ color: 'yellow', 'margin': '20px 20px', textAlign:'center'}}>Sobre o autor</h1>
-          <p style={{color:'white', lineHeight:'1.6', textAlign:'start', textIndent: '40px', padding:'10px', fontSize:'1.25em'}}>Sou graduado em Física Biomolecular pela USP-São Carlos, fiz 1 ano de mestrado com bolsa CAPES porém vi que tenho pontos mais fortes para atuação no mundo empresarial. Então atuei por 3 anos como analista de investimentos e riscos Jr. numa empresa de soluções financeiras que atua através de tecnologia e depois em uma consultoria financeira como consultor independente atuando com prospecção e suporte de clientes. Sou uma pessoa bem comunicativa, que contribui como team player e que busca aprender e evoluir em conhecimento. 
-          </p>
-        </Paper>
-        <br/>
-          <Card style={{maxWidth:'500px', margin:'auto'}}>
-            <CardActionArea>
-              <CardMedia
-                image={Lucas}
-              />
-              <CardContent>
-                <h1>Projeto1</h1>
-                <p color="textSecondary">Aqui a descrição do projeto!</p>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Compartilhar
-              </Button>
-              <Button size="small" color="primary">
-                Visitar
-              </Button>
-            </CardActions>
-          </Card>
-          <br/>
-
-          <Paper elevation={5} style={{ backgroundColor: '#272c34', height: '23vh', width:'90%', margin:"auto" , 'border-radius': '30px', border:'3px solid grey'}}> 
-          <h1 style={{ color: 'yellow', 'margin': '20px 20px', textAlign:'center'}}>Sobre o autor</h1>
-          <p style={{color:'white', lineHeight:'1.6', textAlign:'start', textIndent: '40px', padding:'10px', fontSize:'1.25em'}}>Sou graduado em Física Biomolecular pela USP-São Carlos, fiz 1 ano de mestrado com bolsa CAPES porém vi que tenho pontos mais fortes para atuação no mundo empresarial. Então atuei por 3 anos como analista de investimentos e riscos Jr. numa empresa de soluções financeiras que atua através de tecnologia e depois em uma consultoria financeira como consultor independente atuando com prospecção e suporte de clientes. Sou uma pessoa bem comunicativa, que contribui como team player e que busca aprender e evoluir em conhecimento. 
-          </p>
-        </Paper>
-      </div>
-    </Container>
+    <body style={{'background':'linear-gradient(0deg, rgba(8,8,8,0.6544818611038166) 0%, rgba(0,0,0,0.8701681356136204) 69%, rgba(0,0,0,1) 100%)', 'width':'100vw', height:'212vh'}}>
     </body>
-    <footer style={{background:'black', position:"sticky", height:'50px', bottom:'0', display:'flex', 'flexDirection': 'row', justifyContent:'space-around'}}>
-      <IconButton className="Ref">
+    <footer  className="footer">
+      <IconButton className="button1" >
         <Mail/>
       </IconButton>
-      <IconButton style={{color:"white"}}>
+      <IconButton className="button2">
         <LinkedIn/>
       </IconButton>
-      <IconButton style={{color:"white"}}>
+      <IconButton className="button3">
         <Git/>
       </IconButton>
-      <IconButton style={{color:"white"}}>
+      <IconButton className="button3">
       <ExploreIcon/>
       </IconButton>
-      <IconButton style={{color:"white"}}>
+      <IconButton className="button4">
       <Download/>
       </IconButton>
     </footer>
